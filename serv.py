1625ef044fcffbf193fa8f3549099f9ad861ea29
Zack Traczyk <20495250+xxzbuckxx@users.noreply.github.com>
2023-12-06 20:03:31 -0800

    working view operations

--- serv.py
+++ serv.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify, abort
-from enum import Enum
 import requests
 import time
 import os
@@ -14,7 +13,6 @@ VIEW = VIEW_ENV.split(',')
 CURRENT_ADDRESS = os.environ.get('SOCKET_ADDRESS', '')
 
 # Constants
-REPLICA_OPERATION = Enum('REPLICA_OPERATION', ['ADD', 'DELETE'])
 CONNECTION_TIMEOUT = 5
 LONG_POLLING_WAIT = 3
 
@@ -26,6 +24,11 @@ VECTOR_CLOCK = defaultdict(int)
 # Error Handling
 @app.errorhandler(Exception)
 def handle_exception(error: Exception) -> [{'error': str}, int]:
+    if (not hasattr(error, "code") or not hasattr(error, "description")):
+        app.logger.error("Unexpected error: %s", str(error))
+        return {'error': "unexpected server error"}, 500
+
+    app.logger.error("Unexpected error: %s", error.description)
     return {'error': error.description}, error.code
 
 
@@ -47,13 +50,15 @@ def validate_key_exists(key: str) -> None:
     Keyword arguments:
     key -- the key to be validated
     """
+    global KV_STORE
+
     if key not in KV_STORE:
         abort(404, "Key does not exist")
 
 
 # Helper Functions
 def broadcastReplicaState(broadcastAddress: str,
-                          operation: REPLICA_OPERATION) -> None:
+                          request: str) -> None:
     """
     Brodcast the request to add/delete the new replica IP:PORT to all the other
     replicas.
@@ -62,48 +67,64 @@ def broadcastReplicaState(broadcastAddress: str,
     address -- the IP:PORT of the replica to be added/deleted
     operation -- the operation to be performed on the replica ("ADD" OR "DELETE")
     """
-    if operation not in REPLICA_OPERATION:
+    global CURRENT_ADDRESS, CONNECTION_TIMEOUT, VIEW
+
+    app.logger.debug("current view: %s", VIEW)
+
+    if (len(VIEW) == 1):
+        # Sanity Check
+        assert CURRENT_ADDRESS == VIEW[0], "Only one address in view; should be current address not %s" % VIEW[0]
+
+        app.logger.info(
+            "View only contains current address; skipping broadcast replica state")
+        return
+
+    app.logger.info(
+        f"Broadcast replica state; {request} address {broadcastAddress}")
+
+    if request != "PUT" and request != "DELETE":
         abort(
             500,
-            f"Invalid replica brodcast operation: {operation} not in {REPLICA_OPERATION}")
-
-    # Construct request parameters
-    request = ""
-    endpoint = ""
-    if operation == REPLICA_OPERATION.ADD:
-        request = "PUT"
-        endpoint = "addToReplicaView"
-    elif operation == REPLICA_OPERATION.DELETE:
-        request = "DELETE"
-        endpoint = "deleteFromReplicaView"
-
-    json = {"socket-address": broadcastAddress},
-
-    timeout = (CONNECTION_TIMEOUT, None)
+            f"Invalid replica brodcast operation: {request}")
 
     # Broadcast request to all other replicas
     for address in VIEW:
         if address == CURRENT_ADDRESS:
             continue
 
-        try:
-            response = requests.request(
-                request, f"http://{address}/{endpoint}", json, timeout)
-            response.raise_for_status()
-
-        except requests.exceptions.Timeout as e:
-            # Replica is down; remove from view
-            # TODO: more robust detection mechanism
-            VIEW.remove(address)
-            broadcastReplicaState(address, REPLICA_OPERATION.DELETE)
+        if request == "DELETE" and address == broadcastAddress:
             continue
 
+        try:
+            app.logger.info("Broadcasting replica state to %s", address)
+
+            response = requests.request(
+                request,
+                f"http://{address}/view/nobroadcast",
+                headers={"Content-Type": "application/json"},
+                json={"socket-address": broadcastAddress}
+            )
+
+            response.raise_for_status()
+
+            app.logger.info(
+                f"Successfully broadcasted replica state to {address}")
+
         except requests.exceptions.ConnectionError as e:
-            # TODO: Handle this case; wait for it to come back up? Shutdown
-            # replica?
-            abort(500, f"Current replica {CURRENT_ADDRESS} is down.")
+            # Replica is down; remove from view
+            # TODO: more robust detection mechanism
+            app.logger.error("Could not reach replica %s", address)
+            app.logger.info("Replica %s is down; removing from view", address)
+            VIEW.remove(address)
+            broadcastReplicaState(address, "DELETE")
+            continue
+
+        except requests.exceptions.RequestException as e:
+            abort(e.response.status_code,
+                  f"Unexpected error while brodcasting replica state to {address}: {e}")
 
         except Exception as e:
+            app.logger.error("Unexpected error: %s", e)
             print(
                 f"Unexpected error will long-polling Replica {address}: {e}")
             abort(500, e)
@@ -118,6 +139,7 @@ def broadCastPutKeyReplica(key, value, causalMetadata):
     value -- the value to be added
     causalMetadata -- the causal metadata to be broadcasted
     """
+    global CURRENT_ADDRESS, CONNECTION_TIMEOUT, LONG_POLLING_WAIT, VECTOR_CLOCK, VIEW
     # TODO: detection mechanism for when a replica is down
     for address in VIEW:
         if address == CURRENT_ADDRESS:
@@ -161,32 +183,10 @@ def broadCastPutKeyReplica(key, value, causalMetadata):
 
 
 # Helper routes
-@app.route('/deleteFromReplicaView', methods=['PUT'])
-def deleteFromReplicaView():
-    data = request.get_json()
-    deletedAddress = data['socket-address']
-
-    if deletedAddress not in VIEW:
-        return {"error": "View has no such replica"}, 404
-
-    VIEW.remove(deletedAddress)
-    return {"result": "deleted"}, 200
-
-
-@app.route('/addToReplicaView', methods=['DELETE'])
-def addToReplicaView():
-    data = request.get_json()
-    newAddress = data['socket-address']
-
-    if newAddress in VIEW:
-        return {"result": "already present"}, 200
-
-    VIEW.append(newAddress)
-    return {'result': 'added'}, 201
-
-
 @app.route('/kvs/updateVectorClock', methods=['PUT'])
 def addKeyToReplica():
+    global KV_STORE, VECTOR_CLOCK
+
     data = request.get_json()
     key = data['key']
     value = data['value']
@@ -210,13 +210,15 @@ def addKeyToReplica():
 @app.route('/view', methods=['PUT'])
 def addReplica():
     """"Adds a new replica to the view."""
+    global VIEW
+
     data = request.get_json()
     newAddress = data['socket-address']
 
     if newAddress in VIEW:
         return {"result": "already present"}, 200
 
-    broadcastReplicaState(newAddress, REPLICA_OPERATION.ADD)
+    broadcastReplicaState(newAddress, "PUT")
 
     VIEW.append(newAddress)
 
@@ -226,26 +228,52 @@ def addReplica():
 @app.route('/view', methods=['GET'])
 def getReplica():
     """Retrieve the view from a replica."""
+    global VIEW
+
     return {"view": VIEW}, 200
 
 
 @app.route("/view", methods=['DELETE'])
 def deleteReplica():
     """Removes an existing replica from the view."""
+    global VIEW
+
     data = request.get_json()
     deletedAddress = data['socket-address']
 
     if deletedAddress not in VIEW:
         return {"error": "View has no such replica"}, 404
 
+    broadcastReplicaState(deletedAddress, "DELETE")
+
     VIEW.remove(deletedAddress)
-    broadcastReplicaState(deletedAddress, REPLICA_OPERATION.DELETE)
+
+    return {"result": "deleted"}, 200
+
+
+@app.route('/view/nobroadcast', methods=['DELETE'])
+def deleteFromReplicaView():
+    global VIEW
+
+    app.logger.info("current view: %s", VIEW)
+
+    data = request.get_json()
+    deletedAddress = data['socket-address']
+
+    if deletedAddress not in VIEW:
+        return {"error": "View has no such replica"}, 404
+
+    app.logger.info("about to delete %s", deletedAddress)
+
+    VIEW.remove(deletedAddress)
     return {"result": "deleted"}, 200
 
 
 # Key-Value Store Operations
 @app.route('/kvs/<key>', methods=['PUT'])
 def addKey(key):
+    global KV_STORE, VECTOR_CLOCK
+
     validate_key_length(key)
 
     data = request.get_json()
@@ -268,8 +296,24 @@ def addKey(key):
     return {'result': "replaced", "causal-metadata": VECTOR_CLOCK}, 200
 
 
+@app.route('/view/nobroadcast', methods=['PUT'])
+def addToReplicaView():
+    global VIEW
+
+    data = request.get_json()
+    newAddress = data['socket-address']
+
+    if newAddress in VIEW:
+        return {"result": "already present"}, 200
+
+    VIEW.append(newAddress)
+    return {'result': 'added'}, 201
+
+
 @app.route('/kvs/<key>', methods=['GET'])
 def getKey(key):
+    global KV_STORE, VECTOR_CLOCK
+
     validate_key_exists(key)
 
     data = request.get_json()
@@ -286,6 +330,8 @@ def getKey(key):
 
 @app.route('/kvs/<key>', methods=['DELETE'])
 def deleteKey(key):
+    global KV_STORE
+
     validate_key_exists(key)
 
     data = request.get_json()
@@ -298,7 +344,5 @@ def deleteKey(key):
 
 # Main
 if __name__ == '__main__':
-    print(int(CURRENT_ADDRESS.split(':')[1]))
-    print(CURRENT_ADDRESS.split(':')[0])
     app.run(debug=True, port=int(CURRENT_ADDRESS.split(
         ':')[1]), host=CURRENT_ADDRESS.split(':')[0])
